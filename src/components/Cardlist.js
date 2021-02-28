66270a60c3fee306a5fd9a751e570c5cf21ac5ba
pankaj485 <61234787+pankaj485@users.noreply.github.com>
2021-02-28 23:29:31 +0530

    inital uplod
    added necessary files

---
+++ src/components/Cardlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+	return (
+		<div>
+			{robots.map((item, index) => {
+				return <Card key={index} id={item.id} name={item.name} email={item.email} />;
+			})}
+		</div>
+	);
+};
+
+export default CardList;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import './index.css';
+import App from './containers/App';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+reportWebVitals();
